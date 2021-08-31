1356b0ab2b69e187706656fffe2b29782ef101d5
Josh Kneubuhl <jkneubuh@us.ibm.com>
2021-08-31 08:53:37 -0400

    wip - store MSP assets on localhost
    Signed-off-by: Josh Kneubuhl <jkneubuh@us.ibm.com>

--- fabctl-sandbox/src/test/java/org/hyperledger/fabric/fabctl/v1/TestBase.java
+++ fabctl-sandbox/src/test/java/org/hyperledger/fabric/fabctl/v1/TestBase.java
@@ -7,7 +7,12 @@ package org.hyperledger.fabric.fabctl.v1;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.ConfigBuilder;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
 
 @Slf4j
 public class TestBase
@@ -17,4 +22,23 @@ public class TestBase
     protected static final YAMLMapper yamlMapper = new YAMLMapper();
 
     protected static final String TEST_NAMESPACE = "test-network";
+
+
+    protected static Config kubeConfig;
+
+    protected static KubernetesClient client;
+
+    @BeforeAll
+    public static void beforeAll() throws Exception
+    {
+        kubeConfig = new ConfigBuilder()
+                .withTrustCerts(true)
+                .withNamespace(TEST_NAMESPACE)
+                .build();
+
+        log.info("Connecting to k8s with context:\n{}",
+                 yamlMapper.writeValueAsString(kubeConfig.getCurrentContext()));
+
+        client = new DefaultKubernetesClient(kubeConfig);
+    }
 }
